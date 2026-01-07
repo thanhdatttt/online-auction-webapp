9ee60434ce69ecca2b18f439c613e27c00ec94dd
thanhdatttt <tmaxzv@gmail.com>
2026-01-07 10:14:47 +0700

    change url for deploy

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -4,7 +4,7 @@ import { useAuthStore } from "../stores/useAuth.store.js";
 // set up for calling apis
 const api = axios.create({
   // get the url with correct mode
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
